dd74e88dd9b65e9d58b9ee60b5ad84cf9d4d3daf
Daria <sinitsyna.daria@gmail.com>
2023-02-07 17:41:41 +0300

    Решена задача №10

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает вторую цифру этого
+числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number <= 999 && number >= 100)
+{
+    int a = number / 10;
+    a = a % 10;
+    Console.WriteLine(a);
+}
+else Console.WriteLine("Вы ввели не трехзначное число");
